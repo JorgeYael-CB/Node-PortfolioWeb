4812bafec45b89f1091062440ed21dde90bf83cb
JorgeYael-CB <jorge.yael333@gmail.com>
2024-06-08 10:47:16 -0600

    dtos de las respuestas (los obligatorios) agregados

--- src/domain/dtos/answer/addAnswer.dto.ts
+++ src/domain/dtos/answer/addAnswer.dto.ts
@@ -9,9 +9,16 @@ export class AddAnswerDto {
 
 
   static create( body: { [key:string]:any } ):[string?, AddAnswerDto?] {
+    const { answer, userId } = body;
 
+    if( !answer || !userId ) return['Answer and token is required'];
 
-    return[]
+    if( typeof answer !== 'string' ) return [`type ${typeof answer} is not valid.`];
+
+    if( answer.length <= 5 ) return ['answer is too length'];
+    if( answer.length >= 500 ) return ['answer is too long'];
+
+    return[ undefined, new AddAnswerDto(answer.trim(), userId) ];
   }
 
 }
--- src/domain/dtos/answer/getAnswerBy.dto.ts
+++ src/domain/dtos/answer/getAnswerBy.dto.ts
@@ -3,6 +3,18 @@
 
 export class GetAnswerByDto {
 
-  constructor(){};
+  constructor(
+    public readonly questionId:string,
+    public readonly answerId: string,
+  ){};
+
+
+  static create( body: { [key:string]:any } ):[string?, GetAnswerByDto?] {
+    const { questionId, answerId } = body;
+
+    if( !questionId || ! answerId ) return ['questionId or anwerId is required'];
+
+    return[ undefined, new GetAnswerByDto(questionId, answerId) ];
+  }
 
 }
